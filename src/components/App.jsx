a1d47e485664971583c0b58088cb83a8d0569ebf
Manzana1 <61259393+Manzana1@users.noreply.github.com>
2025-04-28 10:33:16 -0400

    change title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,50 @@
-import { useState, useEffect } from 'react'
-import Banner from './Banner'
-import logo from '../assets/logo.png'
-import Cart from './Cart'
-import Footer from './Footer'
-import ShoppingList from './ShoppingList'
-import '../styles/Layout.css'
-import StockManager from './StockManager'
-import { plantList } from '../datas/plantList'
+import { useState, useEffect } from "react";
+import Banner from "./Banner";
+import logo from "../assets/logo.png";
+import Cart from "./Cart";
+import Footer from "./Footer";
+import ShoppingList from "./ShoppingList";
+import "../styles/Layout.css";
+import StockManager from "./StockManager";
+import { plantList } from "../datas/plantList";
 
 function App() {
-	const savedCart = localStorage.getItem('cart')
-	console.log(savedCart)
-	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
-	const [plants, updatePlants] = useState(plantList)
+  const savedCart = localStorage.getItem("cart");
+  console.log(savedCart);
+  const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : []);
+  const [plants, updatePlants] = useState(plantList);
 
-	console.log('JSON.parse(savedCart)')
+  console.log("JSON.parse(savedCart)");
 
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
+  return (
+    <div>
+      <Banner>
+        <img src={logo} alt="logo-la-maison-jungle" className="lmj-logo" />
+        <h1 className="lmj-title">Jungle House</h1>
+      </Banner>
 
-	useEffect(() => {
-		localStorage.setItem('cart', JSON.stringify(cart))
-	}, [cart])
+      <div className="lmj-layout-inner">
+        <Cart
+          cart={cart}
+          updateCart={updateCart}
+          plants={plants}
+          updatePlants={updatePlants}
+        />
+        <ShoppingList
+          plants={plants}
+          cart={cart}
+          updateCart={updateCart}
+          updatePlants={updatePlants}
+        />
+      </div>
 
-	return (
-		<div>
-			<Banner>
-				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-			</Banner>
-
-			<div className='lmj-layout-inner'>
-
-				<Cart cart={cart} updateCart={updateCart} plants={plants} updatePlants={updatePlants} />
-				<ShoppingList plants={plants} cart={cart} updateCart={updateCart} updatePlants={updatePlants} />
-
-			</div>
-
-
-			<Footer />
-		</div>
-	)
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
